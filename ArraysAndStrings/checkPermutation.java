82d0469b5790426bb901661c9d908f3464ec1758
Shobhit Tripathi <61062688+ShobhitTripathi@users.noreply.github.com>
2021-07-09 20:23:28 +0530

    Update checkPermutation.java

--- ArraysAndStrings/checkPermutation.java
+++ ArraysAndStrings/checkPermutation.java
@@ -5,15 +5,19 @@ import java.util.Arrays;
 public class checkPermutation {
 
     static boolean checkStrings(String s1, String s2) {
+        //base condition
         if (s1.length() != s2.length())
             return false;
         char[] str1 = s1.toCharArray();
         char[] str2 = s2.toCharArray();
+        //count array for String s1
         int[] count1 = new int[256];
         Arrays.fill(count1, 0);
+        //count array for String s2
         int[] count2 = new int[256];
         Arrays.fill(count2, 0);
 
+        //incrementing the count array as per characters
         for (int i = 0; i < s1.length() && i < s2.length(); i++) {
             count1[str1[i]]++;
             count2[str2[i]]++;
