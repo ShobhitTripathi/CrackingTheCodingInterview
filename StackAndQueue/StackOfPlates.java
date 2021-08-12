db9762fa399140fa11095abf5f2ca0a00dcdea11
Shobhit Tripathi <61062688+ShobhitTripathi@users.noreply.github.com>
2021-08-12 08:51:27 +0530

    Add files via upload

---
+++ StackAndQueue/StackOfPlates.java
@@ -0,0 +1,73 @@
+package StackAndQueue;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class StackOfPlates {
+    private static final int STACK_SIZE = 3;
+    private static LinkedList<Stack<Integer>> stacks = new LinkedList<Stack<Integer>>();
+
+    public static void push(int item) {
+        if (stacks.isEmpty() || stacks.getLast().size() >= STACK_SIZE) {
+            Stack<Integer> stack = new Stack<Integer>();
+            stack.push(item);
+            stacks.add(stack);
+        } else {
+            stacks.getLast().push(item);
+        }
+    }
+
+    public Integer pop() {
+        if (stacks.isEmpty())
+            throw new IllegalStateException("Stacks are empty!");
+        Stack<Integer> last = stacks.getLast();
+        int item = last.pop();
+        if (last.isEmpty())
+            stacks.removeLast();
+        return item;
+    }
+
+    public Integer popAt(int index) {
+        if (stacks.isEmpty())
+            throw new IllegalStateException("Stacks are empty!");
+        if (index < 0 || index >= stacks.size())
+            throw new IllegalArgumentException("Invalid index!");
+        int item = stacks.get(index).pop();
+        for (int i = index; i < stacks.size() - 1; ++i) {
+            Stack<Integer> curr = stacks.get(i);
+            Stack<Integer> next = stacks.get(i + 1);
+            curr.push(next.remove(0));
+        }
+        if (stacks.getLast().isEmpty())
+            stacks.removeLast();
+        return item;
+    }
+
+    public static void printStacks() {
+        for (Stack<Integer> stack : stacks) {
+            for (int item : stack)
+                System.out.print(item + " ");
+            System.out.println("[TOP]");
+        }
+    }
+
+    //TEST----------------------------------
+    public static void main(String[] args) {
+        StackOfPlates setOfStacks = new StackOfPlates();
+        setOfStacks.push(1);
+        setOfStacks.push(2);
+        setOfStacks.push(3);
+        setOfStacks.push(4);
+        setOfStacks.push(5);
+        setOfStacks.push(6);
+        setOfStacks.push(7);
+        setOfStacks.push(8);
+        setOfStacks.push(9);
+
+        setOfStacks.printStacks();
+        System.out.println();
+        setOfStacks.popAt(1);
+        setOfStacks.popAt(0);
+        setOfStacks.printStacks();
+    }
+}
