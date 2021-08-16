600e913323cc904e42926b2e8ac5256fd70f2554
Shobhit Tripathi <61062688+ShobhitTripathi@users.noreply.github.com>
2021-08-16 11:31:15 +0530

    Add files via upload

---
+++ StackAndQueue/AnimalShelter/Cat.java
@@ -0,0 +1,13 @@
+package StackAndQueue.AnimalShelter;
+
+public class Cat extends Animals {
+
+    public Cat () {
+        super.setType("Cat");
+    }
+
+    public void meow(){
+        System.out.println("Meow!");
+    }
+
+}
