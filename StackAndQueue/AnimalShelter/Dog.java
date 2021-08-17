55942920aba93cfbd984ec9c8e48cebcc0b8408f
Shobhit Tripathi <61062688+ShobhitTripathi@users.noreply.github.com>
2021-08-17 15:51:58 +0530

    Add files via upload

---
+++ StackAndQueue/AnimalShelter/Dog.java
@@ -0,0 +1,13 @@
+package StackAndQueue.AnimalShelter;
+
+public class Dog extends Animals {
+
+    public Dog () {
+        super.setType("Dog");
+    }
+
+    public void bark() {
+        System.out.println("bark");
+    }
+
+}
