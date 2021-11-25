bc30ae0a562eb075e80d4386a027a7b6c633a45b
Shobhit Tripathi <61062688+ShobhitTripathi@users.noreply.github.com>
2021-11-25 19:38:47 +0530

    Add files via upload

---
+++ ObjectOrientedDesign/CallCentre/Rank.java
@@ -0,0 +1,10 @@
+package ObjectOrientedDesign.CallCentre;
+
+public enum Rank {
+    Respondents, Managers, Directors;
+
+    public int getValue() {
+        //dummy value
+        return 0;
+    }
+}
